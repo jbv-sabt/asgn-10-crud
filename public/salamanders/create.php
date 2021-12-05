ba8c35633b61bec9ff758e2ce94616560f77ac65
jeltjebvellema <jeltjebvellema@students.abtech.edu>
2021-12-04 21:37:41 -0500

    updated  to python2.7, retrieved values from new.php form, called insert_salamander function, retrieved new id and updated redirect to inlcude new id

--- public/salamanders/create.php
+++ public/salamanders/create.php
@@ -5,11 +5,17 @@ require_once('../../private/initialize.php');
 if(is_post_request()) {
 
   // Handle form values sent by new.php
+  $name = $_POST['name'] ?? '';
+  $habitat = $_POST['habitat'] ?? '';
+  $description = $_POST['description'] ?? '';
 
-  $salamanderName = $_POST['salamanderName'] ?? '';
+  $result = insert_salamander($name, $habitat, $description);
+  $new_id = mysqli_insert_id($db);
+  redirect_to(h(url_for('/salamanders/show.php?id=' . $new_id)));
+  
   
   echo "Form parameters<br />";
-  echo "Salamander name: " . $salamanderName . "<br />";
+  echo "Salamander name: " . $name . "<br />";
 } else {
   redirect_to(url_for('salamanders/new.php'));
 }
