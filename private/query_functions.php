ed48bee242191759dbc9496738b4700cb368bd7e
jeltjebvellema <jeltjebvellema@students.abtech.edu>
2021-12-04 21:16:42 -0500

    Added function insert_salamander($name, $habitat, $description)

--- private/query_functions.php
+++ private/query_functions.php
@@ -20,3 +20,27 @@ function find_salamander_by_id($id) {
     mysqli_free_result($result);
     return $salamander; //returns assoc array
 }
+
+function insert_salamander($name, $habitat, $description){
+    global $db;
+    
+    $sql = "INSERT INTO salamander ";
+    $sql .= "(name, habitat, description) ";
+    $sql .= "VALUES (";
+    $sql .= "'" .  $name . "',";
+    $sql .= "'" . $habitat . "',";
+    $sql .= "." . $description . "'";
+    $sql .= ")";
+
+    $result = mysqli_query($db, $sql);
+    //for insert statements, $result returns t/f
+    if($result) {
+        return true;
+    }
+    else {
+    //insert failed
+    echo mysqli_error($db);
+    db_disconnect($db)
+    exit;
+    }
+}
